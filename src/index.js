3591ca1945fc64708cc824f1ea70b02a0661ca21
Pechenyice <german.iskhakov@mail.ru>
2021-12-08 18:52:43 +0300

    completed steps 3 and 4, now it can render in competetive-style

--- src/index.js
+++ src/index.js
@@ -23,19 +23,86 @@ function createTextElement(text) {
   };
 }
 
-function render(element, container) {
+function createDOM(fiber) {
   const dom =
-    element.type === "TEXT_ELEMENT"
+    fiber.type === "TEXT_ELEMENT"
       ? document.createTextNode("")
-      : document.createElement(element.type);
+      : document.createElement(fiber.type);
   const isProperty = (key) => key !== "children";
-  Object.keys(element.props)
+  Object.keys(fiber.props)
     .filter(isProperty)
     .forEach((name) => {
-      dom[name] = element.props[name];
+      dom[name] = fiber.props[name];
     });
-  element.props.children.forEach((child) => render(child, dom));
-  container.appendChild(dom);
+
+  return dom;
+}
+
+function render(element, container) {
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [element],
+    },
+  };
+}
+
+let nextUnitOfWork = null;
+
+function workLoop(deadline) {
+  let shouldYield = false;
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+    shouldYield = deadline.timeRemaining() < 1;
+  }
+  requestIdleCallback(workLoop);
+}
+
+requestIdleCallback(workLoop);
+
+function performUnitOfWork(fiber) {
+  if (!fiber.dom) {
+    fiber.dom = createDOM(fiber);
+  }
+
+  if (fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom);
+  }
+
+  const elements = fiber.props.children;
+  let index = 0;
+  let prevSibling = null;
+
+  while (index < elements.length) {
+    const element = elements[index];
+
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null,
+    };
+
+    if (index === 0) {
+      fiber.child = newFiber;
+    } else {
+      prevSibling.sibling = newFiber;
+    }
+
+    prevSibling = newFiber;
+    index++;
+  }
+
+  if (fiber.child) {
+    return fiber.child;
+  }
+  let nextFiber = fiber;
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling;
+    }
+    nextFiber = nextFiber.parent;
+  }
 }
 
 const EReactile = {
@@ -43,7 +110,18 @@ const EReactile = {
   render,
 };
 
-const element = <h1>Hello World!</h1>;
+const element = (
+  <div>
+    <div>
+      <div>
+        <h1>there</h1>
+        <h3>is</h3>
+        <h6>loop</h6>
+      </div>
+    </div>
+    <h1>Hello World!</h1>
+  </div>
+);
 
 const container = document.getElementById("root");
 EReactile.render(element, container);
